6958ad05616c9ad65fb692ab5632acb2226f3e53
Audun Kvasbø <audun@kvasbo.no>
2011-04-20 14:38:34 +0200

    Refactoring for template use in all functions

--- src/js/functions.js
+++ src/js/functions.js
@@ -247,7 +247,7 @@ function dayClicked(timestamp, force)
 function highLightToday()
 {
 	var today = new Date();
-	today.setSeconds(0, 0).setMinutes(0);
+	today.setSeconds(0, 0);
 	today.setMinutes(0);
 	today.setHours(0);
 	var selectorString = '[dateTimestamp="'+today.getTime()+'"]';
