87f8b3f0b99de0a8f301918d1d0fca1b09b0d58f
Audun Kvasbø <audun@kvasbo.no>
2012-07-31 22:32:17 +0200

    Started work on notes and colors for dates. Mainly in background.js

--- src/js/background.js
+++ src/js/background.js
@@ -2,6 +2,8 @@ var dateArray; //Holds the dates we count down to
 var subDateArray;
 var dateNotes; //Notes for dates
 var newInstall; //Is this a first time install (ie: is the date array set?)
+var dateNoteArray; //Notes for dates
+var dateColorArray; //Colors for dates
 
 /**
 Run maintenance script every minute
@@ -60,6 +62,22 @@ function getSubDates()
 }
 
 /**
+Add or remove a note for a date
+*/
+function setNoteForDate(timestamp, note, remove)
+{
+
+}
+
+/**
+Add or remove a color mark for a date
+*/
+function setColorForDate(timestamp, color, remove)
+{
+
+}
+
+/**
 Toggle dates. "nocount" means secondary dates if true.
 */
 function toggleDate(timestamp, noCount)
@@ -107,107 +125,6 @@ function toggleDate(timestamp, noCount)
 	
 }
 
-
-/**
-Initialise settings and/or reset everything to scratch
-*/ 
-function resetSettings()
-{
-	log("Event", "resetSettings()");
-	
-	dateArray = getItem("dateArray");
-	if(dateArray == null && getItem("countto") != null )
-	{
-		//Transition to new solution for storing date.
-		dateArray = new Array();
-		var countTo = getItem("countto");
-		toggleDate(countTo);
-		log("Migrating date solution", countTo);
-	}
-	else if(dateArray == null)
-	{
-		dateArray = new Array();
-		setItem("dateArray", JSON.stringify(dateArray));
-		log("Setting default (empty) date array", dateArray);
-	}
-	else
-	{
-		dateArray = JSON.parse(dateArray);
-	}
-	
-	//Secondary dates
-	noCountDateArray = getItem("noCountDateArray");
-	if(noCountDateArray == null)
-	{
-		noCountDateArray = new Array();
-		setItem("noCountDateArray", JSON.stringify(noCountDateArray));
-	}
-	else
-	{
-		noCountDateArray = JSON.parse(noCountDateArray);
-	}
-	
-
-	var icon_topColor = getItem("icon_topColor");
-	if(icon_topColor == null)
-	{
-		var icon_topColor = "rgba(27,140,160,1)";
-		setItem("icon_topColor", icon_topColor);
-		log("Setting up default icon top color", icon_topColor);
-	}
-	
-	//Load default icon, autocreates new if not already set
-	var icon = new Icon(new Object());
-	icon.getDefaultValues(true);
-	
-	
-	
-	var showBadge = getItem("showBadge");
-	if(showBadge == null)
-	{
-		var showBadge = "1";
-		setItem("showBadge", showBadge);
-		log("Setting up default badge display", showBadge);
-	}
-	
-	var icon_textColor = getItem("icon_textColor");
-	if(icon_textColor == null)
-	{
-		var icon_textColor = "rgba(0,0,0,0.65)";
-		setItem("icon_textColor", icon_textColor);
-		log("Setting up default icon text color", icon_textColor);
-	}
-
-	var icon_showtext = getItem("icon_showtext");
-	if(icon_showtext == null)
-	{
-		var icon_showtext = "0";
-		setItem("icon_showtext", icon_showtext);
-		log("setting up icon text");
-	}
-	
-	var badgeColor = getItem("badgeColor");
-	if(badgeColor == null) {
-		var color = "#18CD32";
-		setItem("badgeColor", color);
-		log("setting up default badge color");
-	}
-
-	var popup = getItem("popup");
-	if(popup == null) {
-		var popup = "12";
-		setItem("popup", popup);
-		log("setting up default popup");
-	}
-
-	var iconColor = getItem("iconColor");
-	if(iconColor == null) {
-		var popup = "red";
-		setItem("iconColor", popup);
-		log("setting default icon color");
-	}
-
-}
 
 /**
 Convert stored dates to use UTC. One time conversion, but does not screw up on multiple loads. 
@@ -546,6 +463,132 @@ function bginit()
 	resetSettings();
 	
 	maintainLoop();
+}
+
+/**
+Initialise settings and/or reset everything to scratch
+*/ 
+function resetSettings()
+{
+	log("Event", "resetSettings()");
+	
+	dateArray = getItem("dateArray");
+	if(dateArray === null && getItem("countto") != null )
+	{
+		//Transition to new solution for storing date.
+		dateArray = new Array();
+		var countTo = getItem("countto");
+		toggleDate(countTo);
+		log("Migrating date solution", countTo);
+	}
+	else if(dateArray === null)
+	{
+		dateArray = new Array();
+		setItem("dateArray", JSON.stringify(dateArray));
+		log("Setting default (empty) date array", dateArray);
+	}
+	else
+	{
+		dateArray = JSON.parse(dateArray);
+	}
+	
+	
+	//Init date note array
+	dateNoteArray = getItem("dateNoteArray");
+	if(dateNoteArray == null)
+	{
+		dateNoteArray = new Array();
+		setItem("dateNoteArray", JSON.stringify(dateNoteArray));
+		//log("Debug", "Setting default (empty) note array");
+	}
+	else {
+		dateNoteArray = JSON.parse(dateNoteArray);
+	}
+	
+	
+	//Init date color array
+	dateColorArray = getItem("dateColorArray");
+	if(dateColorArray == null)
+	{
+		dateColorArray = new Array();
+		setItem("dateColorArray", JSON.stringify(dateColorArray));
+	}
+	else {
+		dateColorArray = JSON.parse(dateColorArray);
+	}
+	
+	//Secondary dates
+	noCountDateArray = getItem("noCountDateArray");
+	if(noCountDateArray === null)
+	{
+		noCountDateArray = new Array();
+		setItem("noCountDateArray", JSON.stringify(noCountDateArray));
+	}
+	else
+	{
+		noCountDateArray = JSON.parse(noCountDateArray);
+	}
+	
+
+	var icon_topColor = getItem("icon_topColor");
+	if(icon_topColor == null)
+	{
+		var icon_topColor = "rgba(27,140,160,1)";
+		setItem("icon_topColor", icon_topColor);
+		log("Setting up default icon top color", icon_topColor);
+	}
+	
+	//Load default icon, autocreates new if not already set
+	var icon = new Icon(new Object());
+	icon.getDefaultValues(true);
+	
+	
+	
+	var showBadge = getItem("showBadge");
+	if(showBadge == null)
+	{
+		var showBadge = "1";
+		setItem("showBadge", showBadge);
+		log("Setting up default badge display", showBadge);
+	}
+	
+	var icon_textColor = getItem("icon_textColor");
+	if(icon_textColor == null)
+	{
+		var icon_textColor = "rgba(0,0,0,0.65)";
+		setItem("icon_textColor", icon_textColor);
+		log("Setting up default icon text color", icon_textColor);
+	}
+
+	var icon_showtext = getItem("icon_showtext");
+	if(icon_showtext == null)
+	{
+		var icon_showtext = "0";
+		setItem("icon_showtext", icon_showtext);
+		log("setting up icon text");
+	}
+	
+	var badgeColor = getItem("badgeColor");
+	if(badgeColor == null) {
+		var color = "#18CD32";
+		setItem("badgeColor", color);
+		log("setting up default badge color");
+	}
+
+	var popup = getItem("popup");
+	if(popup == null) {
+		var popup = "12";
+		setItem("popup", popup);
+		log("setting up default popup");
+	}
+
+	var iconColor = getItem("iconColor");
+	if(iconColor == null) {
+		var popup = "red";
+		setItem("iconColor", popup);
+		log("setting default icon color");
+	}
+
 }
 
 
