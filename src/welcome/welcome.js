ee073890087d530d8401a7eea143b20ee15fe9c7
Audun Kvasbø <audun@kvasbo.no>
2012-10-10 16:02:58 +0200

    Removed tracking from popup to speed up performance

--- src/welcome/welcome.js
+++ src/welcome/welcome.js
@@ -7,21 +7,13 @@
 			hoverPause: true,
 			animationStart: function(current){
 				$('.caption').animate({
-					bottom:-35
+					bottom:-210
 				},100);
-				if (window.console && console.log) {
-					// example return of current slide number
-					console.log('animationStart on slide: ', current);
-				};
 			},
 			animationComplete: function(current){
 				$('.caption').animate({
 					bottom:0
 				},200);
-				if (window.console && console.log) {
-					// example return of current slide number
-					console.log('animationComplete on slide: ', current);
-				};
 			},
 			slidesLoaded: function() {
 				$('.caption').animate({
