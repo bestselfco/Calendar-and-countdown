1983df5f4b1a02b24a09f650d64ff3bad725aafa
Audun Kvasbø <Audun@Auduns-MacBook-Pro.local>
2016-04-13 13:36:34 +0200

    2016.4.13.3
    Remove heartbeat.

--- src/js/googletrack.js
+++ src/js/googletrack.js
@@ -4,19 +4,6 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // Note: https protocol here
 
-var intervalTracking = setInterval(trackPing, 4*60*1000);
-
-function trackPing()
-{
-	try{
-		ga('send', 'event', 'System', "Ping", "");
-	}
-	catch(e)
-	{
-		trackError("background", "tracking", "trackPing");
-	}
-}
-
 /**
 Track a page view to Google
 */
