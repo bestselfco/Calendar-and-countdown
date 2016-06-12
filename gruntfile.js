e65c1773e42467e50512c1fe915f145b8037e865
Audun Kvasbø <audun@kvasbo.no>
2016-06-12 09:25:30 +0200

    jQuery 3

--- gruntfile.js
+++ gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     manifest: grunt.file.readJSON('src/manifest.json'),
     pkg: grunt.file.readJSON('package.json'),
     filesBackground: ['src/js/libraries/jquery.js', 'src/js/googletagmanager.js', 'src/js/libraries/jworkflow-min-0.7.0.js', "src/js/functions.js", "src/js/date.extend.js", "src/js/external_common.js","src/js/icons.js","src/js/migrations.js","src/js/googletrack.js","src/js/background.js", "src/js/debug.js"],
-    filesCalendar: ['src/js/libraries/jquery.js', 'src/js/googletagmanager.js', 'src/js/libraries/mustache.js', 'src/js/libraries/jworkflow-min-0.7.0.js', 'src/js/libraries/tipped/js/tipped/tipped.unpacked.js', 'src/js/date.extend.js', 'src/js/templates.js', 'src/js/external_common.js', 'src/js/functions.js','src/js/tooltips_tipped.js','src/js/debug.js','src/js/googletrack_toback.js', 'src/js/calendar.js'],
+    filesCalendar: ['src/js/libraries/jquery.js','src/js/googletagmanager.js', 'src/js/libraries/mustache.js', 'src/js/libraries/jworkflow-min-0.7.0.js', 'src/js/libraries/tipped/js/tipped/tipped.unpacked.js', 'src/js/date.extend.js', 'src/js/templates.js', 'src/js/external_common.js', 'src/js/functions.js','src/js/tooltips_tipped.js','src/js/debug.js','src/js/googletrack_toback.js', 'src/js/calendar.js'],
     filesOptions: ['src/js/libraries/jquery.js', 'src/js/googletagmanager.js', 'src/js/libraries/jquery-ui/**/*.js','src/js/date.extend.js', 'src/js/libraries/jworkflow-min-0.7.0.js', 'src/js/external_common.js', 'src/js/functions.js', 'src/js/icons.js',  'src/js/migrations.js', 'src/js/debug.js', 'src/js/googletrack_toback.js', 'src/js/options.js'],
     filesGuide: ['src/userguide/cc_guide.pdf'],
     cssOptions: ['src/css/cssreset.css', 'src/css/options.css'],
